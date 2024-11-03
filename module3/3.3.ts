757b241e9222fdaa72b95274cdd0445bb0410906
zesanahmed <zesanahmed593@gmail.com>
2024-11-03 23:15:09 +0600

    type guard

---
+++ module3/3.3.ts
@@ -0,0 +1,46 @@
+{
+  // typeof --> type guard
+
+  type Alphanumeric = string | number;
+
+  const add = (param1: Alphanumeric, param2: Alphanumeric): Alphanumeric => {
+    if (typeof param1 === "number" && typeof param2 === "number") {
+      return param1 + param2;
+    } else {
+      return param1.toString() + param2.toString();
+    }
+  };
+
+  const result1 = add(5, 5);
+  console.log(result1);
+
+  type NormalUser = {
+    name: string;
+  };
+
+  type AdminUser = {
+    name: string;
+    role: "admin";
+  };
+
+  const getUser = (user: NormalUser | AdminUser) => {
+    if ("role" in user) {
+      console.log(`My name is ${user.name} & my role is ${user.role}`);
+    } else {
+      console.log(`My name is ${user.name}`);
+    }
+  };
+
+  const normalUser: NormalUser = {
+    name: "Mr. Normal bro",
+  };
+
+  const adminUser: AdminUser = {
+    name: "Mr. Admin bro",
+    role: "admin",
+  };
+
+  getUser(adminUser);
+
+  //
+}
